f635c53c474baaf4a9880f967b2a97c158fe1417
Monsterdeveloper01 <farelsfadlillah@gmail.com>
2026-01-12 12:54:16 +0700

    nama dah

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,28 +4,28 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
-    // public function RouteJualBeliMotor() { // Modul
+    // public function RouteJualBeliMotor() { // Modul: YANG TERUTAMA YAZID tapi ttp bareng bareng
     //     return view('jualbelimotor.index');
     // }
     
-    public function RouteSparePartMotor() { //
+    public function RouteSparePartMotor() { // Bintang
         return view('sparepart.index');
     }
 
-    public function RouteAksesorisMotor() {
+    public function RouteAksesorisMotor() { // Farel
      return view('aksesoris.index');
     }
 
-    public function RouteServisMotor() {
+    public function RouteServisMotor() { // Yazid
         return view('servismotor.index');
 
     }
 
-    public function RouteSteamMotor() {
+    public function RouteSteamMotor() { // bintang
         return view('steammotoor.index');
     }
 
-    public function RouteModfikasiMotor() {
+    public function RouteModfikasiMotor() { // Farel
         return view('modifikasi.index');
     }
 
